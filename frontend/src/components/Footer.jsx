894a9746b06b7d67b0e3bc5613f0f0ddf1fff617
Akriti321 <166243384+Akriti321@users.noreply.github.com>
2026-05-30 21:56:20 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'> Doctor Desk is a modern healthcare platform that connects patients with trusted medical professionals. We simplify the appointment booking process by allowing users to discover specialists, view doctor profiles, and schedule consultations online. Our goal is to make quality healthcare more accessible, convenient, and efficient for everyone.
+</p>
         </div>
 
         <div>
@@ -24,8 +25,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+91-9131369842</li>
+            <li>DoctorDesk@gmail.com</li>
           </ul>
         </div>
 
@@ -33,7 +34,7 @@ const Footer = () => {
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2026 @ DoctorDesk.com - All Right Reserved.</p>
       </div>
 
     </div>
